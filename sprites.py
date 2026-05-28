6c4b7ca90ef05036856ca1fec03394184aa023b3
Daniele Del Giudice <daniele@delgiudice.dev>
2026-05-28 21:24:06 +0200

    chore: :lipstick: formatting pass

--- sprites.py
+++ sprites.py
@@ -1,23 +1,36 @@
-import pygame, os
+import pygame
+import os
 from pygame.locals import *
 import math
+
+
 class SpriteCarta(pygame.sprite.Sprite):
-    def __init__(self,carta,posizione):
+    def __init__(self, carta, posizione):
         pygame.sprite.Sprite.__init__(self)
-        dir_image = "res" + os.sep + "carte" + os.sep + str(carta.seme) + str(carta.valore-1) + ".gif"
+        dir_image = (
+            "res"
+            + os.sep
+            + "carte"
+            + os.sep
+            + str(carta.seme)
+            + str(carta.valore - 1)
+            + ".gif"
+        )
         self.image = pygame.image.load(dir_image).convert()
         self.rect = self.image.get_rect()
         self.rect.topleft = posizione
         self.carta = carta
-    def draw(self,screen):
-        screen.blit(self.image,self.rect)
-    def update(self,rel):
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def update(self, rel):
         self.rect = self.rect.move(rel)
+
     def move(self, screen, pos):
         new_x, new_y = pos
-        cur_x , cur_y = self.rect.topleft
+        cur_x, cur_y = self.rect.topleft
         dif_x, dif_y = (cur_x - new_x), (cur_y - new_y)
-        i = math.sqrt(pow((dif_x),2) + pow((dif_y),2))
+        i = math.sqrt(pow((dif_x), 2) + pow((dif_y), 2))
         if dif_y < dif_x:
             pass
-        
